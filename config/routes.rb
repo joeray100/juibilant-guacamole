42923026422329bd25030102d8f7949cb50baca7
Joe Ray <jray100.jr@gmail.com>
2021-04-27 10:06:11 -0600

    us_2 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :gardens
-  resources :plots
-  resources :plot_plants
-  resources :plants
+  resources :plots, only: [:index] do
+    resources :plants, only: [:destroy]
+  end
+
+  resources :gardens, only: [:show]
 end
--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -30,4 +30,20 @@ RSpec.describe 'Plot Index Page' do
     expect(page).to have_content(@plant_2.name)
     expect(page).to have_content(@plant_3.name)
   end
+
+  it "Next to each plant I see a link to remove that plant from that plot" do
+    expect(page).to have_link("Delete", href: "/plots/#{@plot_1.id}/plants/#{@plant_1.id}")
+  end
+
+  it "I can delete a plots plant" do
+    click_link 'Delete', match: :first
+    
+    expect(current_path).to eq("/plots")
+    expect(page).to_not have_content(@plant_1.name)
+    expect(page).to_not have_link('Delete',count: 3)
+  end
+  # When I click on that link
+  # I'm returned to the plots index page
+  # And I no longer see that plant listed under that plot
+  # (Note: you should not destroy the plant record entirely)
 end
