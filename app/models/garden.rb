d7bbf14867e2da73a518cdd97775478bb9986d91
Joe Ray <jray100.jr@gmail.com>
2021-04-27 10:37:09 -0600

    us_3 complete

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,4 +2,8 @@ class Garden < ApplicationRecord
   has_many :plots
   has_many :plot_plants, through: :plots
   has_many :plants, through: :plot_plants
+
+  def unique_and_ordered_plants
+    plants.select('plants.*').where('days_to_harvest < ?', 100).distinct
+  end
 end
