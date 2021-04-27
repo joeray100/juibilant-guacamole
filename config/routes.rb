bdf7a3a894a55168c8a0d14b52da0ab72563acfc
Joe Ray <jray100.jr@gmail.com>
2021-04-27 11:53:22 -0600

    update us_2 to delete correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :plots, only: [:index] do
-    resources :plants, only: [:destroy]
-  end
-
+  resources :plots, only: [:index]
   resources :gardens, only: [:show]
+  delete "/plots/:plot_id/plants/:plant_id", to: "plot_plants#destroy"
 end
--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -3,17 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Plot Index Page' do
   before :each do
     @garden_1 = Garden.create!(name: "Turing Community Garden", organic: true)
+    @garden_2 = Garden.create!(name: "Denver local Garden", organic: true)
 
     @plot_1 = @garden_1.plots.create!(number: 25, size: "Large", direction: "East")
+    @plot_2 = @garden_2.plots.create!(number: 34, size: "Large", direction: "East")
 
     @plant_1 = @plot_1.plants.create!(name: "Purple Beauty Sweet Bell Pepper", description: "Prefers rich, well draining soil.", days_to_harvest: 90)
     @plant_2 = @plot_1.plants.create!(name: "Onions", description: "Prefers wet soil.", days_to_harvest: 150)
     @plant_3 = @plot_1.plants.create!(name: "Watermelon", description: "Prefers dry soil.", days_to_harvest: 50)
-
-    @garden_2 = Garden.create!(name: "Denver local Garden", organic: true)
-
-    @plot_2 = @garden_2.plots.create!(number: 34, size: "Large", direction: "East")
-
     @plant_4 = @plot_2.plants.create!(name: "Chilis", description: "Prefers dry weather.", days_to_harvest: 75)
     @plant_5 = @plot_2.plants.create!(name: "Carrots", description: "Prefers rainy weather.", days_to_harvest: 125)
     @plant_6 = @plot_2.plants.create!(name: "Mint", description: "Prefers topical conditions.", days_to_harvest: 60)
@@ -31,15 +28,12 @@ RSpec.describe 'Plot Index Page' do
     expect(page).to have_content(@plant_3.name)
   end
 
-  it "Next to each plant I see a link to remove that plant from that plot" do
-    expect(page).to have_link("Delete", href: "/plots/#{@plot_1.id}/plants/#{@plant_1.id}")
-  end
-
   it "I can delete a plots plant" do
-    click_link 'Delete', match: :first
+      expect(page).to have_content(@plant_1.name)
+      click_link 'Delete', match: :first
 
-    expect(current_path).to eq("/plots")
-    expect(page).to_not have_content(@plant_1.name)
-    expect(page).to_not have_link('Delete',count: 3)
+      expect(current_path).to eq("/plots")
+      expect(page).to_not have_content(@plant_1.name)
+      expect(page).to_not have_link('Delete',count: 3)
   end
 end
